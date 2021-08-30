5617dd4e69a95942c4e0fae77a59516ad718ae2e
Benson Misi <benson.misi@outlook.com>
2021-08-30 15:55:37 +0200

    product view changes

--- resources/views/shopview.blade.php
+++ resources/views/shopview.blade.php
@@ -10,10 +10,25 @@
     <li class="breadcrumb-item active" aria-current="page">Product View</li>
   </ol>
 </nav>
+<div class="page_section">
     <div class="row">
+        
+        <div class="col-md-10 offset-md-1">
+        <div class="row d-flex">
+        <div class="@if(count($cart)==0) col-md-12  @else col-md-9 @endif">
         <x-product :product="$product" :cart="$cart"/>
+        </div>
+        @if(count($cart)>0)
+        <div class="col-md-3">  
         <x-mycart :cart="$cart" :currency="$currency"/>
+        </div>
+        @endif
+        </div>
+
+        </div>
     </div>
+</div>
+    
 
 </div>
 
