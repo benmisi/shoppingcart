f2b66300a9e2f8b84c87a03909a6ffa6523c8140
Benson Misi <benson.misi@outlook.com>
2021-08-30 16:33:31 +0200

    admin area changes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -89,6 +89,9 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
+                                    <a class="nav-link" href="{{route('admin.home')}}"><i class="fa fa-cogs" aria-hidden="true"></i>Admin</a>
+                        </li>
+                        <li class="nav-item">
                                     <a class="nav-link" href="{{route('Admin-customers.index')}}"> <i class="fa fa-users"></i> Customers</a>
                         </li>
                         <li class="nav-item">
@@ -110,12 +113,9 @@
                         <li class="nav-item">
                                     <a class="nav-link" href="{{route('Admin-currency.index')}}"><i class="fa fa-money" aria-hidden="true"></i> Currency Settings</a>
                         </li>
+                      
     
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
         </div>
